dcd17d1844a90c925b185dbc251a2564452733a5
DaryRodriguez <dariannyrodriguez@liceovvh.cl>
2024-03-18 11:18:45 -0300

    ok

--- Fundamentos_JS/01_ejercicios/script.js
+++ Fundamentos_JS/01_ejercicios/script.js
@@ -1,1 +1,33 @@
-console.log("Hola mundo")
+//Una variable global de tipo var para mostrar el alcance global de la variable.
+//Una variable local de tipo let para mostrar el alcance local de la variable.
+//Una constante mes para demostrar un dato no modificable.
+
+
+var global = "Global";
+let local = "Local";
+const constante = "Constante";
+
+console.log("Variables: Global:" + global + " - Local:" + local + " - Constante:" + constante);
+
+let num = 4;
+let numeroDecimal = 3.1416;
+let string = "Texto";
+let booleano = true;
+
+console.log("Variables: Numero Decimal:" + numeroDecimal + " - Cadena de Texto :" + string + " - Booleano:" + booleano)
+
+let array=["Perro",20, "Gato",32]; 
+let objeto={edad: 17,
+     nombre: "Dary",
+    telefono: "93099302" 
+}; 
+let indefinida;
+let nula=null;
+
+ function  sumar(num1, num2) {
+  
+    return num1 + num2;
+};
+console.log("Variables:\nEntero: " + num +"n\numeroDecimal: " + numeroDecimal + "\nstring: "+ string + "\nbooleano: "+ booleano +"\narray: "+ array +"\nobjeto: "+ JSON.stringify(objeto) +"\nindefinida: " + indefinida + "\nula: "+ nula + "\nResultado de función : " + sumar );
+console.log(sumar(10,5));
+
---
+++ Fundamentos_JS/02_ejercicios/script.js
@@ -0,0 +1,59 @@
+console.log("Vinculado correctamente")
+// Ejercicio 1: Declaración de variables
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
+// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
+
+function declararVariables() {
+    let ahora =2024
+    let fecha_nac=2006
+    resultado_edad=2024-2006
+  alert("Mi edad es"+ resultado_edad);
+
+    }
+    
+    // Ejercicio 2: Asignación de valores
+    // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'
+    function asignarValores() {
+     // Tu código aquí
+     let=var1
+     let=var2
+
+     alert("El valor de var 2 es " + var2);
+    
+    }
+    
+    // Ejercicio 3: Operaciones matemáticas
+    // Instrucciones: Completa la función para realizar las siguientes operaciones:
+    // Suma 'num1' y 'num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
+    // Muestra los resultados en un mensaje.
+    function realizarOperaciones() {    
+    // Tu código aquí
+    let num1=4
+    let num2=5
+    let num3=6
+    let suma = 4 + 5 + 6;
+    let resta = 4 - 6;
+    let multiplicacion = 5 * 6;
+    let divicion = 4 / 120;
+
+    alert("la suma es " + suma + "\n la resta es " + resta + "\n la multiplicacion es " + multiplicacion + "\n la divicion es " + divicion );
+
+
+
+
+
+    }
+    
+    // Ejercicio 4: Manipulación de cadenas
+    // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
+    // Luego, muestra la longitud del mensaje y el antepenultimo carácter en el mensaje.(.length)
+    function manipularCadenas() {
+        let direccion = "San Simon , Gran Av";
+        let n_casa = 502;
+        let mensaje = "Mi direccion es: " + direccion + " " + n_casa;
+        let longitud = mensaje.length;
+        alert("Mensaje: " + mensaje +"\nLongitud: " + longitud +"\nPrimer Caracter: " + mensaje[0]);
+        // 
+    }
+
+    
